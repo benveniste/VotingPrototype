ab039f8fcdff3454cfa7193d713851a7057f64a7
mhb <mhb@dossier.us>
2025-02-03 11:27:13 -0500

    Smart Contract Wiring works.

--- src/main/solidity/xmlContract.sol
+++ src/main/solidity/xmlContract.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
+// solc ./src/main/solidity/xmlContract.sol --bin --abi --optimize -o ./src/main/resources/ --overwrite
+// web3j generate solidity -b ./src/main/resources/xmlStorage.bin -a ./src/main/resources/xmlStorage.abi -o ./src/main/java -p com.smofs
+
 pragma solidity >=0.4.16 <0.9.0;
 
-contract SimpleStorage {
+contract xmlStorage {
     string private storedXml;
 
     function set(string memory xml) public {
--- src/test/kotlin/com/smofs/wsfs/blockchain/BlockTest.kt
+++ src/test/kotlin/com/smofs/wsfs/blockchain/BlockTest.kt
@@ -1,19 +1,28 @@
 package com.smofs.wsfs.blockchain
 
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import com.smofs.XmlStorage
 import com.smofs.wsfs.aws.AwsApi
 import io.grpc.Grpc
 import io.grpc.TlsChannelCredentials
 import org.hyperledger.fabric.client.identity.Identities
 import org.hyperledger.fabric.client.identity.Signers
 import org.hyperledger.fabric.client.identity.X509Identity
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.web3j.crypto.WalletUtils
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.http.HttpService
+import org.web3j.tx.RawTransactionManager
+import org.web3j.tx.gas.DefaultGasProvider
 import software.amazon.awssdk.utils.StringInputStream
+import java.io.File
 
 
 class BlockTest {
     @Test
+    @Disabled
     fun fetchTest() {
         val tlsCert = AwsApi.getSecret("WSFS-Blockchain-TLS")
         var identityCert = AwsApi.getSecret("WSFS-Blockchain-Identity")
@@ -35,9 +44,20 @@ class BlockTest {
 
     @Test
     fun web3jTest() {
-        val web3 = Web3j.build(HttpService("http://localhost:8545"))
-        val web3ClientVersion = web3.web3ClientVersion().send()
+        val web3j = Web3j.build(HttpService("http://localhost:8545"))
+        val web3ClientVersion = web3j.web3ClientVersion().send()
         val clientVersion = web3ClientVersion.web3ClientVersion
         requireNotNull(clientVersion)
+        val credMap = AwsApi.getSecretMap("WSFS-Blockchain")
+
+        val resourceUrl = javaClass.getClassLoader().getResource("AccountKey.json")!!
+        val credentials = WalletUtils.loadCredentials(credMap["AccountPassword"], File(resourceUrl.file))
+        val chainId = web3j.ethChainId().send().chainId.toLong()
+        val mangler = RawTransactionManager(web3j, credentials, chainId)
+        val smartContract = XmlStorage.deploy(web3j, mangler, DefaultGasProvider()).send()
+        val result = smartContract.set("<MHB></MHB>").send()
+        var fetch =  smartContract.get().send()
+        requireNotNull(result)
+        assertThat("Bad fetch", fetch, equalTo("<MHB></MHB>"))
     }
 }
